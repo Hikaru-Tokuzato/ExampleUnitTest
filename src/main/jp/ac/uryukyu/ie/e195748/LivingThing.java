ecb2651ebd04355c713bab337f90ca806be07639
Hikaru Tokuzato <e195748@ie.u-ryukyu.ac.jp>
2019-11-25 22:11:40 +0900

    step2 commit

---
+++ src/main/jp/ac/uryukyu/ie/e195748/LivingThing.java
@@ -0,0 +1,33 @@
+package jp.ac.uryukyu.ie.e195748;
+
+public class LivingThing {
+
+    String name;
+    int hitPoint;
+    int attack;
+    boolean dead;
+
+    public boolean isDead() {
+        return dead;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void attack(LivingThing opponent) {
+        if (!dead) {
+            int damage = (int) (Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+            opponent.wounded(damage);
+        }
+    }
+
+    public void wounded(int damage) {
+        hitPoint -= damage;
+        if( hitPoint < 0 ) {
+            dead = true;
+            System.out.printf("%sは倒れた。\n", name);
+        }
+    }
+}
